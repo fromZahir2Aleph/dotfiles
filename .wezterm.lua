0268b9e2ca708ae9f6fb21de6159d83529a8efd4
back2analogue <zelim.alaudinov@gmail.com>
2023-10-08 19:52:05 +0300

    chore: Back up

---
+++ .wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+local act = wezterm.action
+
+return {
+  default_prog = { '/usr/bin/zsh', '-l' },
+  color_scheme = 'GruvboxDark',
+  font = wezterm.font('FiraCode Nerd Font'),
+  font_size = 12,
+  line_height = 1.1,
+}
+
